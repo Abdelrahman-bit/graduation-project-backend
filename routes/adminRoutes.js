0eda998a678ec1a8e88baeaaeb1aed036d168d2b
Abdelrahman-bit <abdolela670@gmail.com>
2025-12-13 21:56:29 +0200

    feat: add comprehensive admin functionalities for managing applications, courses, users, and dashboard statistics

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -5,7 +5,20 @@ import {
    updateCourseStatus,
    getInReviewCourses,
    searchInstructors,
+   getDashboardStats,
+   getAllCourses,
+   getAllStudents,
+   getInstructor,
+   deleteStudent,
+   getStudent,
 } from '../controller/adminControllers.js';
+import {
+   getHalls,
+   newHall,
+   getHallDetails,
+   updateHallDetails,
+   deleteHall,
+} from '../controller/hallControllers.js';
 import auth from '../middleware/authentication.js';
 import restrictTo from '../middleware/authorization.js';
 
@@ -17,6 +30,7 @@ router.use(restrictTo('admin'));
 // authentication routes
 // Application Requests Routes for instructor applications
 router.get('/dashboard/applicationRequest', getApplicationRequests);
+router.get('/dashboard/overview', getDashboardStats);
 // update application status route
 router
    .route('/dashboard/applicationRequest/:id')
@@ -24,6 +38,20 @@ router
 
 router.patch('/courses/:courseId/status', updateCourseStatus);
 router.get('/courses/review', getInReviewCourses);
+router.get('/courses', getAllCourses);
+router.get('/students', getAllStudents);
+router.get('/students/:id', getStudent);
+router.delete('/students/:id', deleteStudent);
 router.get('/instructors', searchInstructors);
+router.get('/instructors/:id', getInstructor);
+
+// Hall Management Routes
+router.route('/halls').get(getHalls).post(newHall);
+
+router
+   .route('/halls/:id')
+   .get(getHallDetails)
+   .patch(updateHallDetails)
+   .delete(deleteHall); // Soft Delete
 
 export default router;
